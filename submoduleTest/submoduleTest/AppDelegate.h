83f17bc5ccc66d0395177e880e3a18e264f1dd2b
chinbin <1249677128@qq.com>
2022-03-24 11:06:33 +0800

    修改：1.项目创建并提交；

---
+++ submoduleTest/submoduleTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  submoduleTest
+//
+//  Created by linqipeng on 2022/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ submoduleTest/submoduleTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  submoduleTest
+//
+//  Created by linqipeng on 2022/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
