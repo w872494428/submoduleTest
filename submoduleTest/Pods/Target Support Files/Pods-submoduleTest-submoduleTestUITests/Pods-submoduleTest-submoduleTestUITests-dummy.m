e1d1b470acbf945c5715f43dbf71fa00c8dbca73
chinbin <1249677128@qq.com>
2022-03-27 16:06:46 +0800

    修改：1.添加pod常用第三方；

---
+++ submoduleTest/Pods/Target Support Files/Pods-submoduleTest-submoduleTestUITests/Pods-submoduleTest-submoduleTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_submoduleTest_submoduleTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_submoduleTest_submoduleTestUITests
+@end
