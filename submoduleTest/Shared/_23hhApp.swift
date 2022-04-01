09c8d269ca9b996f2f3f1e2c7bb264d98f77774e
chinbin <1249677128@qq.com>
2022-04-01 10:14:44 +0800

    修改：1.Package包新增文件；

---
+++ submoduleTest/Shared/_23hhApp.swift
@@ -0,0 +1,17 @@
+//
+//  _23hhApp.swift
+//  Shared
+//
+//  Created by linqipeng on 2022/3/31.
+//
+
+import SwiftUI
+
+@main
+struct _23hhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
